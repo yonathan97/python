62a450a53685249b2bce35b1d6352e356cad8f69
Yonathan Shiferaw <yonathan.shiferaw97@gmail.com>
2022-02-13 09:13:33 +0300

    2nd

---
+++ inputs.py
@@ -0,0 +1,5 @@
+name = input("Please, enter your name: ")
+age = input("Please, enter your age: ")
+
+
+print ("You are ", name, str(age))
---
+++ shapes.py
@@ -0,0 +1,15 @@
+print("       /| ")
+print("      / |")
+print("     /  |")
+print("    /   |")
+print("   /    |")
+print("  /     |")
+print(" /      |")
+print("/_______|")
+character_name = "Yonathan"
+character_age = 12
+print (character_name," is ", character_age)
+print (character_name +"+ is ",character_age)
+
+print(character_name.islower())
+print(character_name.index("Y"))
